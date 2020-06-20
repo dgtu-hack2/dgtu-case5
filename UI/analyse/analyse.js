a00ef7fadbd6abf05aa58d6f0b28ed4090cd087c
Алексей Жуков <gkvalex7@gmail.com>
2020-06-20 11:04:05 +0300

    analyse

--- UI/analyse/analyse.js
+++ UI/analyse/analyse.js
@@ -3,7 +3,16 @@
 var analyse = angular.module('myApp.analyse', ['ngRoute']);
 
 analyse.controller('AnalyseCtrl', function ($scope, programService, $q) {
+    getPrograms();
 
-    
+    function getPrograms() {
+        programService.getPrograms().then(function (response) {
+            var data = response;
 
+            if (data[0].Progs) {
+                console.log(data[0].Progs)
+                $scope.programs = data[0].Progs;
+            }
+        });
+    }
 });
