3fa24ea5ac922bd327e777ed31bc5a26f8c57066
Алексей Жуков <gkvalex7@gmail.com>
2020-06-20 13:09:28 +0300

    Update translate.py

--- translate.py
+++ translate.py
@@ -44,7 +44,7 @@ def compare(link):
     content = str(BeautifulSoup(get_html(link), "lxml").get_text()).splitlines()
     prepared = []
     for item in content:
-        if len(item):
+        if len(item.strip()):
             prepared.append(item.strip())
     
     # сравниваем
@@ -61,16 +61,21 @@ def compare(link):
         for el in re.split('\W+', ln, flags=re.UNICODE):
             if len(el) > 2:
                 dr_kw2[el] = dr_kw2.get(el, 0) + 1
+
     aa = []
     for el in set.union(set(dr_kw.keys()), set(dr_kw2.keys())):
         x1 = dr_kw.get(el, 0)
         x2 = dr_kw2.get(el, 0)
-        if (x1 > 5) | (x2 > 5):
+        if (x1 > 5) or (x2 > 5):
             aa.append([x1, x2])
     aa = np.array(aa).T
+
+    if all(v == 0 for v in aa[0]) or all(v == 0 for v in aa[1]):
+        return 0
+
     return 1 - spatial.distance.cosine(aa[0], aa[1])
 
 
 if __name__ == "__main__":
     translate('168.63.61.94', 27017, '5eed22a7b7da9028b46cfd2b')
-    compare(sys.argv[1])
+    print(compare(sys.argv[1]))
